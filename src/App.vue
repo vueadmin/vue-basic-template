d18c800fda575c7bbb631ff6de60a8e5d16a5c58
zhangchuang <698088330@qq.com>
2024-12-10 13:48:24 +0800

    设置 element-plus 中文模式

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,13 @@
+<script setup>
+import zhCn from 'element-plus/lib/locale/lang/zh-cn'
+
+const locale = zhCn
+</script>
+
 <template>
-  <main>
-    <router-view />
-  </main>
+  <div id="app">
+    <el-config-provider :locale="locale">
+      <router-view />
+    </el-config-provider>
+  </div>
 </template>
--- src/components.d.ts
+++ src/components.d.ts
@@ -7,6 +7,7 @@ export {}
 /* prettier-ignore */
 declare module 'vue' {
   export interface GlobalComponents {
+    ElConfigProvider: typeof import('element-plus/es')['ElConfigProvider']
     ElIcon: typeof import('element-plus/es')['ElIcon']
     ElMenu: typeof import('element-plus/es')['ElMenu']
     ElMenuItem: typeof import('element-plus/es')['ElMenuItem']
